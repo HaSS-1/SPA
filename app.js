b6d387560325614dc80a5f6b6b961954b3412c18
t.hassan <tajiri.hassan@gmail.com>
2022-03-31 00:07:15 +0200

    :tada: index.html done | app.js sone, and media queries

--- app.js
+++ app.js
@@ -4,23 +4,21 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 
-function page_Transitions() {
-    //button click active class
+function PageTransitions() {
+    //Button click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
             this.className += ' active-btn';
         })
     }
 
-    //Sections Active
-
+    //Sctions Active 
     allSections.addEventListener('click', (e) => {
-        //console.log(e.target)
         const id = e.target.dataset.id;
         if (id) {
-            //remove selected from the other btns
+            //resmove selected from the other btns
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active')
             })
@@ -30,11 +28,18 @@ function page_Transitions() {
             sections.forEach((section) => {
                 section.classList.remove('active')
             })
+
             const element = document.getElementById(id);
             element.classList.add('active');
         }
     })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
-
-page_Transitions();
+PageTransitions();
